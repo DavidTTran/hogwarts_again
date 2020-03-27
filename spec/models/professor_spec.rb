46b0ab36b115760fdd1d97a7a285121306170afc
David Tran <davidttemail@gmail.com>
2020-03-27 10:33:24 -0600

    Update professor model test with average_student_age

--- spec/models/professor_spec.rb
+++ spec/models/professor_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe Professor, type: :model do
     it {should have_many :professor_students}
     it {should have_many(:students).through(:professor_students)}
   end
+
+  describe ".average_student_age" do
+    it "should calculate average of professors students" do
+      lupin = Professor.create(name: "Remus Lupin", age: 49 , specialty: "Defense Against The Dark Arts")
+      harry = Student.create(name: "Harry Potter" , age: 11 , house: "Gryffindor" )
+      malfoy = Student.create(name: "Draco Malfoy" , age: 12 , house: "Slytherin" )
+      ProfessorStudent.create(student_id: harry.id, professor_id: lupin.id)
+      ProfessorStudent.create(student_id: malfoy.id, professor_id: lupin.id)
+
+      expect(lupin.average_student_age).to eq(11.5)
+    end
+  end
 end
